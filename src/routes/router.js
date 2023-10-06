22a03a5037d6866469d9e36a9ca611533c37ac91
deynertorrado <deynertorrado1@gmail.com>
2023-10-06 01:54:16 -0500

    New Changes: Add Cows in a SupaBase

--- src/routes/router.js
+++ src/routes/router.js
@@ -25,11 +25,37 @@ router.post("/api/login", async (req, res) => {
         res.status(404).json({
             success: false,
             message: "Usuario no registrado",
+            error
         });
     } else {
         res.status(200).send(data);
     }
 });
+
+// POST: Crear nueva vaquita en Supabase
+router.post("/api/cows", async (req, res) => {
+    const Name = req.body.cowName;
+    const Code = req.body.cowCode;
+    const Breed = req.body.cowBreed;
+    const Date = req.body.cowDate;
+    const Weight = req.body.cowWeight;
+    const Births = req.body.cowBirths;
+
+    const { data, error } = await supabase
+        .from('vaquitas')
+        .insert({cowName: Name, cowCod: Code, cowBreed: Breed, cowDate: Date, cowWeight: Weight, cowBirths: Births})
+        .select()
+
+    if (data == null) {
+        res.status(404).json({
+            success: false,
+            message: "No se pudo agregar la vaquita al corral",
+            error
+        });
+    } else {
+        res.status(200).send(data);
+    }
+})
 
 // Exportamos el router al index.js
 export default router;
