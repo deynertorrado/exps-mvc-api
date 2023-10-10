677aa2bb92b8218c40dd98bc39da606cbc53ce5d
deynertorrado <deynertorrado1@gmail.com>
2023-10-10 14:49:38 -0500

    Add put method for the users

--- src/routes/router.js
+++ src/routes/router.js
@@ -157,5 +157,31 @@ router.get("/api/users", async (req, res) => {
     }
 })
 
+// PUT: Crear nuevo usuario en Supabase
+router.put("/api/users", async (req, res) => {
+    const userName = req.body.userName;
+    const user = req.body.user;
+    const userPassword = req.body.userPassword;
+    const userType = req.body.userType;
+    const id = req.body.userId
+
+    const { data, error } = await supabase
+        .from('perfiles')
+        .update([{ name: userName, username: user, password: userPassword, type: userType }])
+        .eq('id', id)
+        .select()
+
+    if (data == null) {
+        res.status(404).json({
+            success: false,
+            message: "Ocurrió un error al actualizar los datos",
+            error
+        });
+    } else {
+        res.status(200).send(data);
+    }
+})
+
+
 // Exportamos el router al index.js
 export default router;
